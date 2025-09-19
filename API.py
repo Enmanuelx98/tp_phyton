537af63ef26594107b8fcbefd7dfff2967f5994e
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-09-18 19:12:57 -0500

    Optimización en FastAPI v6

--- API.py
+++ API.py
@@ -125,7 +125,7 @@ def evaluate_video(video_path, threshold=0.8, min_frames=5, delay_frames=3):
 
     # liberar memoria manualmente
     gc.collect()
-    tf.keras.backend.clear_session()
+    #tf.keras.backend.clear_session()
 
     return [pred_word] if pred_word else []
 
